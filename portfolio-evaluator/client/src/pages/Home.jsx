95128eaa2c992d9c0d7fa0d5f1685731a8ed7223
MOUNI47 <108328202+MOUNI47@users.noreply.github.com>
2026-03-23 20:48:20 +0530

    Add files via upload

---
+++ portfolio-evaluator/client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Portfolio Evaluator 🚀</h2>
+      <p>Enter GitHub username to analyze profile</p>
+    </div>
+  );
+}
+
+export default Home;
